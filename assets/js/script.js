49627eb95ff8f4b543d53aa8016e7aa2e2c0365c
Specialist001 <darking-uz@yandex.ru>
2019-05-17 13:05:05 +0500

    fix programs.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    var menu_offset_top = $('.main-header').outerHeight();
+    var menu_offset_top = $('.main.main-header').outerHeight();
     var checkWidths = jQuery(window).width();
     function processScroll() {
         var scrollTop = $( window ).scrollTop();
         if ((scrollTop >= menu_offset_top)&&checkWidths >= 768) {
-            $('.main-header').addClass('menu-fixed');
+            $('.main.main-header').addClass('menu-fixed');
             $('#logo').attr('src', 'assets/images/png/logo_black.png');
-            
 
-        } else if (scrollTop <= menu_offset_top) {
-            $('.main-header').removeClass('menu-fixed');
+        } else if (scrollTop < menu_offset_top) {
+            $('.main.main-header').removeClass('menu-fixed');
             $('#logo').attr('src', 'assets/images/png/logo_white.png');
             $('.navbar-toggler-icon').css("background-image: url(../images/svg/menu.svg)");
         }
